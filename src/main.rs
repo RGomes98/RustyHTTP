28a1a743331c0c78538f96e0b8441ff304d464ab
Renan Gomes <renango2015@gmail.com>
2025-03-19 03:23:09 -0300

    build(env): add env variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::net::TcpListener;
+use std::env;
+
+fn get_from_env(key:&str, fallback:&str) -> String {
+    return env::var(key).unwrap_or(fallback.to_string());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let host: String = get_from_env("HOST", "localhost");
+    let port: String = get_from_env("PORT", "3000");
+    let listener: TcpListener = TcpListener::bind(format!("{host}:{port}")).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
