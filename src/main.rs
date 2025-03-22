bfc49b4dbfa7d29f875ba469559b3828b7cc0554
Renan Gomes <renango2015@gmail.com>
2025-03-21 21:42:02 -0300

    feat(server): add error handling to server listener

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,20 @@ use modules::logger::Logger;
 use std::net::TcpListener;
 
 fn main() {
-    let host: String = Env::get_env_var_or_default("HOST", "localhost");
-    let port: String = Env::get_env_var_or_default("PORT", "3000");
+    let host: String = Env::get_env_var_or_default("HOST", "127.0.0.1");
+    let port: String = Env::get_env_var_or_default("PORT", "8080");
 
     let listener: TcpListener = TcpListener::bind(format!("{host}:{port}")).unwrap();
     Logger::info(&format!("Server is now listening on {host}:{port}."));
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection established!");
+        match stream {
+            Ok(stream) => {
+                Logger::info("Connection established!");
+            }
+            Err(e) => {
+                Logger::error(&format!("Failed to accept connection: {e}"));
+            }
+        }
     }
 }
