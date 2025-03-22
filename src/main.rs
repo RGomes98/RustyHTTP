7b464ee456c927a6f9e30e2e8cb043a11634b5aa
Renan Gomes <renango2015@gmail.com>
2025-03-21 21:13:06 -0300

    chore(gitignore): update gitignore; minor code changes

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use modules::logger::Logger;
 use std::net::TcpListener;
 
 fn main() {
-    let host: String = Env::get_env_var_or_default("HOaST", "localhost");
-    let port: String = Env::get_env_var_or_default("PaORT", "3000");
+    let host: String = Env::get_env_var_or_default("HOST", "localhost");
+    let port: String = Env::get_env_var_or_default("PORT", "3000");
 
     let listener: TcpListener = TcpListener::bind(format!("{host}:{port}")).unwrap();
     Logger::info(&format!("Server is now listening on {host}:{port}."));
