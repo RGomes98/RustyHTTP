3f565cc84ff372fb299215c64434250e02a8e226
Renan Gomes <renango2015@gmail.com>
2025-04-03 23:34:04 -0300

    feat(status_code): add 'http_status_code' module

--- src/modules/http/mod.rs
+++ src/modules/http/mod.rs
@@ -1,6 +1,7 @@
 pub mod http_method;
 pub mod http_server;
+pub mod http_status_codes;
 pub mod request;
 pub mod response;
 
-pub use self::{http_method::*, http_server::*, request::*, response::*};
+pub use self::{http_method::*, http_server::*, http_status_codes::*, request::*, response::*};
