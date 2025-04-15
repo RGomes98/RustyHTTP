802b54e1281fe9f3b71921a52d4410e44751388b
Renan Gomes <renango2015@gmail.com>
2025-04-15 01:27:05 -0300

    feat(env): improve 'Env' module, add parsing using generics

--- src/modules/config/env.rs
+++ src/modules/config/env.rs
@@ -1,17 +1,32 @@
 use crate::modules::utils::Logger;
 
 use std::env;
+use std::str::FromStr;
 
 pub struct Env;
+
 impl Env {
-    pub fn get_env_var_or_exit(variable_name: &str) -> String {
-        Self::get_env_var(variable_name).unwrap_or_else(|err: String| {
+    pub fn get_parsed<T>(variable_name: &str) -> T
+    where
+        T: FromStr,
+        T::Err: std::fmt::Display,
+    {
+        let raw_str: String = Self::get(variable_name).unwrap_or_else(|err: String| {
             Logger::error(&format!("Failed to retrieve environment variable: {err}"));
             std::process::exit(1)
-        })
+        });
+
+        raw_str
+            .parse::<T>()
+            .unwrap_or_else(|err: <T as FromStr>::Err| {
+                Logger::error(&format!(
+                    "Failed to parse environment variable '{variable_name}': {err}."
+                ));
+                std::process::exit(1)
+            })
     }
 
-    fn get_env_var(var: &str) -> Result<String, String> {
+    fn get(var: &str) -> Result<String, String> {
         match env::var(var) {
             Ok(value) if !value.trim().is_empty() => Ok(value),
             Ok(_) => Err(format!("Invalid format for environment variable '{var}'.")),
