cbf7f20b4dedf433709bb639807bf49d4928eb77
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 06:56:20 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 466ab09776f0e15b0abcd51590950834b4848361

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768546580804,
+  "repoUrl": "https://github.com/RGomes98/RustyHTTP",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "54034976+RGomes98@users.noreply.github.com",
+            "name": "Renan Tavares",
+            "username": "RGomes98"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "466ab09776f0e15b0abcd51590950834b4848361",
+          "message": "Merge pull request #1 from RGomes98/ci-benchmark\n\nAdd benchmark workflow",
+          "timestamp": "2026-01-16T03:55:02-03:00",
+          "tree_id": "e1ccc9d963ea8615feb6cb4662abb448248654bc",
+          "url": "https://github.com/RGomes98/RustyHTTP/commit/466ab09776f0e15b0abcd51590950834b4848361"
+        },
+        "date": 1768546580313,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "API Throughput",
+            "value": 15345.75,
+            "unit": "req/s"
+          }
+        ]
+      }
+    ]
+  }
+}
