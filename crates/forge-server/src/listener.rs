5de1061a015ef57d9680ef0e9849d424a3da46cd
Renan Gomes <renango2015@gmail.com>
2026-02-06 17:39:24 -0300

    spike(server): inicial listener structure with monoio

--- crates/forge-server/src/listener.rs
+++ crates/forge-server/src/listener.rs
@@ -1,17 +1,24 @@
 use std::io::Error;
 use std::net::{Ipv4Addr, SocketAddr};
+use std::num::NonZero;
 use std::sync::Arc;
+use std::thread::{self, JoinHandle};
 
 use super::{Connection, ListenerError};
 use forge_http::Response;
 use forge_logging::init_logger;
 use forge_router::Router;
-use tokio::net::{TcpListener, TcpStream};
-use tracing::{debug, error, info, warn};
+use monoio::net::{TcpListener, TcpStream};
+use monoio::time::TimeDriver;
+use monoio::{FusionDriver, FusionRuntime, IoUringDriver, LegacyDriver, RuntimeBuilder};
+use tracing::{error, info, warn};
+
+const DEFAULT_RING_ENTRIES: u32 = 4096;
 
 pub struct ListenerOptions {
     pub port: u16,
     pub host: Ipv4Addr,
+    pub threads: usize,
 }
 
 pub struct Listener {
@@ -36,40 +43,89 @@ impl Listener {
         self
     }
 
-    pub async fn run(self) -> Result<(), Error> {
+    pub fn run(self) {
         let address: SocketAddr = SocketAddr::from((self.options.host, self.options.port));
-        debug!("Binding TCP listener to {address}");
+        let shared_router: Arc<Router> = self.router;
 
-        let listener: TcpListener = TcpListener::bind(address).await?;
         info!("Listener running on http://{address}");
 
-        loop {
-            match listener.accept().await {
-                Ok((stream, _address)) => {
-                    let router: Arc<Router> = self.router.clone();
+        let threads: usize = if self.options.threads == 0 {
+            thread::available_parallelism()
+                .map(|n: NonZero<usize>| n.get())
+                .unwrap_or_else(|e: Error| {
+                    warn!("Failed to determine available parallelism: {e}. Defaulting to 1 thread.");
+                    1
+                })
+        } else {
+            self.options.threads
+        };
 
-                    if let Err(e) = stream.set_nodelay(true) {
-                        warn!("Failed to set 'TCP_NODELAY': {e}");
-                    }
+        let handles: Vec<JoinHandle<()>> = (0..threads)
+            .map(|i: usize| {
+                let router_ref: Arc<Router> = shared_router.clone();
 
-                    tokio::spawn(async move { Self::handle_connection(stream, router).await });
-                }
-                Err(e) => {
-                    error!("Failed to accept connection: {e}");
-                }
+                thread::spawn(move || {
+                    let mut runtime: FusionRuntime<TimeDriver<IoUringDriver>, TimeDriver<LegacyDriver>> =
+                        match RuntimeBuilder::<FusionDriver>::new()
+                            .enable_all()
+                            .with_entries(DEFAULT_RING_ENTRIES)
+                            .build()
+                        {
+                            Ok(runtime) => runtime,
+                            Err(e) => {
+                                error!("Failed to build Monoio runtime for thread {i}: {e}");
+                                return;
+                            }
+                        };
+
+                    runtime.block_on(async {
+                        let listener: TcpListener = match TcpListener::bind(address) {
+                            Ok(listener) => listener,
+                            Err(e) => {
+                                error!("Bind error on thread {i}: {e}");
+                                return;
+                            }
+                        };
+
+                        loop {
+                            match listener.accept().await {
+                                Ok((stream, _address)) => {
+                                    let thread_router: Arc<Router> = router_ref.clone();
+
+                                    if let Err(e) = stream.set_nodelay(true) {
+                                        warn!("Failed to set 'TCP_NODELAY' on thread {i}: {e}");
+                                    }
+
+                                    monoio::spawn(async move {
+                                        Self::handle_connection(stream, thread_router).await;
+                                    });
+                                }
+                                Err(e) => {
+                                    error!("Failed to accept connection on thread {i}: {e}");
+                                }
+                            }
+                        }
+                    });
+                })
+            })
+            .collect();
+
+        for (i, handler) in handles.into_iter().enumerate() {
+            if let Err(e) = handler.join() {
+                error!("Thread {i} failed to join: {e:?}");
             }
         }
     }
 
     async fn handle_connection(stream: TcpStream, router: Arc<Router>) {
-        let mut handler: Connection = Connection { router, stream };
+        let mut connection: Connection = Connection { router, stream };
 
         loop {
-            if let Err(e) = handler.process_request().await {
+            if let Err(e) = connection.process_request().await {
                 match e {
                     ListenerError::ConnectionClosed => break,
                     ListenerError::Http(e) => {
-                        if (Response::new(e.status).send(&mut handler.stream).await).is_err() {
+                        if (Response::new(e.status).send(&mut connection.stream).await).is_err() {
                             break;
                         }
                     }
